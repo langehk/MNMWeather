b2f925094c28c558494594f5d0ec5ef908b265f0
langehk <32093132+langehk@users.noreply.github.com>
2020-11-03 12:25:34 +0100

    weatherDataAllOver

--- main.js
+++ main.js
@@ -1,93 +1,91 @@
 import { getNewContent } from './ajax/ajax.js';
 
+'use strict'
+
 let apiKey = "1c40c518571d79e7f81134c6c8e517ba";
 let divelm = document.getElementById("weatherInfo");
 let advDivElm = document.getElementById("advWeatherInfo");
-let lang = "&lang=en"; // Sprog. (Dansk = da)
+let lang = "&lang=da"; // Sprog. (Dansk = da)
 
 let advancedButton = document.getElementById("advancedButton");
-let pCity = document.getElementById("city");
-let pTemp = document.getElementById("temp");
-let pWind = document.getElementById("wind");
-let pMain = document.getElementById("main");
 let lat;
 let long; 
+let weatherInfo; 
 
 function convertToCelcius(x){
-    let kelvin = 273;
+    let kelvin = 273.15;
     let temp = x - kelvin; 
     temp = Math.round(temp);
     return temp;
 }
 
+/*
+Her skriver vi vores data ud 
+*/
 let txtHandler = function(e) {
     /* ajax load event
      * puts received text 
      * onto the dom 
      * into the dom
      */
-    divelm.innerHTML="";
-    let obj = JSON.parse(e.target.responseText);    // objectify response
+    
+    weatherInfo = JSON.parse(e.target.responseText);    // objectify response
                                                     // and use it
-
-    let city = document.createTextNode("City: " + obj.name + " "); 
-    let temp = document.createTextNode("Tempareture: " + convertToCelcius(obj.main.temp) + " °C "); 
-    let wind = document.createTextNode("Wind speed: " + Math.round(obj.wind.speed) + " m/s ");
-    let main = document.createTextNode("Condition: " + obj.weather[0].main);
-
-    lat = obj.coord.lat; // Sætter latitude
-    long = obj.coord.lon; // Sætter longitude
+    let desc = weatherInfo.weather[0].description;
     
-    pCity.appendChild(city); 
-    pTemp.appendChild(temp);
-    pWind.appendChild(wind);
-    pMain.appendChild(main);
-
-    divelm.appendChild(pCity);
-    divelm.appendChild(pTemp);
-    divelm.appendChild(pWind);
-    divelm.appendChild(pMain);
+    divelm.innerHTML =`
+    <p id="city">By: ${weatherInfo.name}</p>
+    <p id="temp">Temperatur: ${convertToCelcius(weatherInfo.main.temp)}°</p>
+    <p id="wind">Vindhastighed: ${Math.round(weatherInfo.wind.speed)} m/s </p>
+    <p id="condition">Vejrforhold: ${desc.charAt(0).toUpperCase() + desc.slice(1)}</p>`;  //Api'et sender den med lille begyndelsesbogstav, derfor ændrer vi det
+    lat = weatherInfo.coord.lat; // Sætter latitude
+    long = weatherInfo.coord.lon; // Sætter longitude
 
 }
 
+
+/*
+Her skriver vi mere info
+*/
 let advHandler = function(e){
-    let obj = JSON.parse(e.target.responseText);
-    
-    let uv = document.createTextNode("Uv index: " + Math.round(obj.value));
-
-    advDivElm.appendChild(uv);
-
+    let obj = JSON.parse(e.target.responseText);  
+    advDivElm.innerHTML = `
+    <p id="uv"> UV-index: ${Math.round(obj.value)}</p>
+    <p id="lufttryk"> Lufttryk: ${Math.round(weatherInfo.main.pressure)} hPa</p>
+    <p id="fugt"> Luftfugtighed: ${Math.round(weatherInfo.main.humidity)}%</p>`;
 }
 
+/*
+Funktionen, der kører, når vi klikker på søg. Her henter vi info fra API'et
+*/
 let getBasicWeather = function () {
-    advDivElm.innerHTML = "";
+
     document.getElementById("searchButton").addEventListener("click", function() {
-        let searchLocation = document.getElementById("searchLocation").value;
-        getNewContent(`http://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid=${apiKey}${lang}`, txtHandler);
-        advancedButton.hidden = false; // Viser advanced button.
+
+    divelm.innerHTML="";
+    advDivElm.innerHTML = "";
+
+    let searchLocation = document.getElementById("searchLocation").value;
+    getNewContent(`http://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid=${apiKey}${lang}`, txtHandler);
+    advancedButton.hidden = false; // Viser advanced button.
     });
-    
+
 }
 
-let getUVIndex = function() {
+/*
+Her henter vi UV-index fra API'et
+*/
+let getAdvancedWeather = function() {
     document.getElementById("advancedButton").addEventListener("click",function() {
         advancedButton.hidden = true; //Skjuler knappen.
         getNewContent(`http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${apiKey}`, advHandler);
     });
 }
 
-/*
-let getAdvancedWeather = function () {
-    document.getElementById("advanced").addEventListener("click", function() {
-        let searchLocation = document.getElementById("searchLocation").value;
-        getNewContent(`http://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid=${apiKey}`, advHandler);
-    });
-}*/
 
 let showStarter = function(){
-    //getAdvancedWeather();
+    getAdvancedWeather();
     getBasicWeather(); 
-    getUVIndex();
 }
 
 window.addEventListener("load", showStarter);                  // kick off JS
